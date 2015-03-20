e186fa1317adf50d65553e47709d0f848a5b7c38
Rick Fuentes <ricardo.fuentes@upr.edu>
2015-03-19 23:19:53 -0400

    added some optional semi colons

--- Web/server/sea_server.js
+++ Web/server/sea_server.js
@@ -1,18 +1,18 @@
-var express = require('express')
+var express = require('express');
 var app = express()
 
 var pg = require('pg');
 var conString = "postgres://username:password@localhost/database";
 
 app.get('/', function (req, res) {
-  res.send('Hello World! This is the reengineering SEA server')
+  res.send('Hello World! This is the reengineering SEA server');
 })
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-  console.log('Sea app listening at http://%s:%s', host, port)
+  console.log('Sea app listening at http://%s:%s', host, port);
 
 })
