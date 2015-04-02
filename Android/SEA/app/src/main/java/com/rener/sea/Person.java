53639011ef8b1c7559b86001719cb2f6c9b324bc
Nelson Reyes <nelson.reyes@upr.edu>
2015-04-02 14:36:14 -0400

    People view

--- Android/SEA/app/src/main/java/com/rener/sea/Person.java
+++ Android/SEA/app/src/main/java/com/rener/sea/Person.java
@@ -4,20 +4,23 @@ public class Person {
 
 	private long id;
 	private String first_name = "";
-	private String middle_initial = null;
-	private String last_name = "";
-	private String email = null;
-	private int phone_number;
+	private String middle_name = "";
+	private String last_name1 = "";
+	private String last_name2 = "";
+	private String email = "";
+	public static final int DEFAULT_PHONE = 0;
+	private int phone_number = DEFAULT_PHONE;
+
 
 	public Person(long id, String first_name, String last_name) {
 		this.id = id;
 		this.first_name = first_name;
-		this.last_name = last_name;
+		this.last_name1 = last_name;
 	}
 
 	public Person(String first_name, String last_name) {
 		this.first_name = first_name;
-		this.last_name = last_name;
+		this.last_name1 = last_name;
 	}
 
 	public long getID() {
@@ -32,27 +35,39 @@ public class Person {
 		return this.first_name = name;
 	}
 
-	public String getMiddleInitial() {
-		return middle_initial;
+	public String getMiddleName() {
+		return middle_name;
 	}
 
-	public String setMiddleInitial(String initial) {
-		return this.middle_initial = initial;
+	public String setMiddleName(String initial) {
+		return this.middle_name = initial;
 	}
 
-	public String getLastName() {
-		return last_name;
+	public boolean hasMiddleName() {
+		return middle_name != null || !middle_name.equals("");
 	}
 
-	public String setLastName(String name) {
-		return this.last_name = name;
+	public String getLastName1() {
+		return last_name1;
+	}
+
+	public String setLastName1(String name) {
+		return this.last_name1 = name;
+	}
+
+	public String getLastName2() {
+		return last_name2;
+	}
+
+	public String setLastName2(String name) {
+		return this.last_name2 = name;
 	}
 
 	public String getFullNameFirstLast() {
-		if(middle_initial != null)
-			return (first_name+" "+middle_initial+". "+last_name).trim();
+		if(hasMiddleName())
+			return (first_name+" "+middle_name+" "+last_name1+" "+last_name2).trim();
 		else
-			return (first_name+" "+last_name).trim();
+			return (first_name+" "+last_name1+" "+last_name2).trim();
 	}
 
 	public String getEmail() {
@@ -63,16 +78,28 @@ public class Person {
 		return this.email = email;
 	}
 
-	public int getPhone_number() {
+	public int getPhoneNumber() {
 		return phone_number;
 	}
 
 	public boolean hasEmail() {
-		return email==null || email.equals("");
+		return email!=null || !email.equals("");
 	}
 
-	public int setPhone_number(int phone_number) {
+	public int setPhoneNumber(int phone_number) {
 		return this.phone_number = phone_number;
+	}
+
+	public int setPhoneNumber(String phone_number) {
+		try {
+			return this.phone_number = Integer.parseInt(phone_number);
+		} catch (NumberFormatException e) {
+			return this.phone_number = DEFAULT_PHONE;
+		}
+	}
+
+	public boolean hasPhoneNumber() {
+		return phone_number != DEFAULT_PHONE;
 	}
 
 	public String toString() {
--- Android/SEA/app/src/main/java/com/rener/sea/PersonDetailsFragment.java
+++ Android/SEA/app/src/main/java/com/rener/sea/PersonDetailsFragment.java
@@ -1,19 +1,25 @@
 package com.rener.sea;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
-public class PersonDetailsFragment extends Fragment {
+public class PersonDetailsFragment extends Fragment implements View.OnClickListener {
 
+	public static final int SHOW_PERSON_LAYOUT = 0;
+	public static final int EDIT_PERSON_LAYOUT = 1;
 	private long person_id;
 	private Person person;
+	private ViewFlipper flipper;
+	private TextView textName, textEmail, textPhoneNumber;
+	private EditText editFirstName, editMiddleName, editLastName1, editLastName2;
+	private EditText editEmail, editPhoneNumber;
+
 
 	public PersonDetailsFragment() {
 		this.person = null;
@@ -43,36 +49,108 @@ public class PersonDetailsFragment extends Fragment {
 	                         Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.person_details_fragment, container, false);
 
-		//Display the person's name
-		String name = person.getFullNameFirstLast();
-		TextView nameText = (TextView) view.findViewById(R.id.text_person_name);
-		nameText.setText(name);
+		//Set the flipper that switches show and edit views
+		flipper = (ViewFlipper) view.findViewById(R.id.person_flipper);
 
-		//Display the person's email
-		String email = person.getEmail();
-		if(email != null) {
-			TextView emailText = (TextView) view.findViewById(R.id.text_person_email);
-			emailText.setText(email);
-			emailText.setVisibility(TextView.VISIBLE);
-		}
+		//Set the name views
+		textName = (TextView) view.findViewById(R.id.text_person_name);
+		editFirstName = (EditText) view.findViewById(R.id.person_edit_first_name);
+		editMiddleName = (EditText) view.findViewById(R.id.person_edit_middle_name);
+		editLastName1 = (EditText) view.findViewById(R.id.person_edit_last_name1);
+		editLastName2 = (EditText) view.findViewById(R.id.person_edit_last_name2);
 
-		String phone = Integer.toString(person.getPhone_number());
-		if(phone != null) {
-			TextView phoneText = (TextView) view.findViewById(R.id.text_person_phone);
-			phoneText.setText(phone);
-			phoneText.setVisibility(TextView.VISIBLE);
-		}
+		//Set the email views
+		textEmail = (TextView) view.findViewById(R.id.text_person_email);
+		editEmail = (EditText) view.findViewById(R.id.person_edit_email);
+
+		//Set the phone number views
+		textPhoneNumber = (TextView) view.findViewById(R.id.text_person_phone);
+		editPhoneNumber = (EditText) view.findViewById(R.id.person_edit_phone_number);
+
+		//Populate the fields
+		setFields();
+
+		//Set the button listeners
+		view.findViewById(R.id.button_edit_person).setOnClickListener(this);
+		view.findViewById(R.id.button_save_person).setOnClickListener(this);
 
 		return view;
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.button_edit_person :
+				flipToEditLayout();
+				break;
+			case R.id.button_save_person :
+				getFields();
+				setFields();
+				flipToShowLayout();
+				break;
+		}
+	}
+
+	private void setFields() {
+
+		//Set the name fields
+		textName.setText(person.getFullNameFirstLast());
+		editFirstName.setText(person.getFirstName());
+		editMiddleName.setText(person.getMiddleName());
+		editLastName1.setText(person.getLastName1());
+		editLastName2.setText(person.getLastName2());
+
+		//Set the email fields if it exists
+		if(person.hasEmail()) {
+			String email = person.getEmail();
+			textEmail.setText(email);
+			editEmail.setText(email);
+			textEmail.setVisibility(TextView.VISIBLE);
+		}
+		else {
+			textEmail.setVisibility(TextView.GONE);
+		}
+
+		//Set the phone fields if it exists
+		if(person.hasPhoneNumber()) {
+			String phone = Integer.toString(person.getPhoneNumber());
+			textPhoneNumber.setText(phone);
+			textPhoneNumber.setVisibility(TextView.VISIBLE);
+			editPhoneNumber.setText(phone);
+		}
+		else {
+			textPhoneNumber.setVisibility(TextView.GONE);
+		}
+
+		flipToShowLayout();
 
 	}
 
-	public void editDetails(View view) {
-
+	private void flipToEditLayout() {
+		flipper.setDisplayedChild(EDIT_PERSON_LAYOUT);
 	}
 
-	public void saveDetails(View view) {
+	private void flipToShowLayout() {
+		flipper.setDisplayedChild(SHOW_PERSON_LAYOUT);
+	}
 
+	private void getFields() {
+
+		//Get the text from the fields
+		String strFirstName = editFirstName.getText().toString();
+		String strMiddleName = editMiddleName.getText().toString();
+		String strLastName1 = editLastName1.getText().toString();
+		String strLastName2 = editLastName2.getText().toString();
+		String strEmail = editEmail.getText().toString();
+		String strPhone = editPhoneNumber.getText().toString();
+
+		//Set the person instance fields
+		person.setFirstName(strFirstName);
+		person.setMiddleName(strMiddleName);
+		person.setLastName1(strLastName1);
+		person.setLastName2(strLastName2);
+		person.setEmail(strEmail);
+		person.setPhoneNumber(strPhone);
 	}
 
 	public static PersonDetailsFragment newInstance(Person person) {
@@ -83,5 +161,4 @@ public class PersonDetailsFragment extends Fragment {
 		fragment.setArguments(args);
 		return fragment;
 	}
-
 }
