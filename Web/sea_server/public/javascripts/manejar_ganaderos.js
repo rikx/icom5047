d9c5bb84ce5d1da88061590ec553b6c2d43a0998
Rick Fuentes <ricardo.fuentes@upr.edu>
2015-04-04 11:24:29 -0400

    Removed some unused code in manejar_ganaderos

--- Web/sea_server/public/javascripts/manejar_ganaderos.js
+++ Web/sea_server/public/javascripts/manejar_ganaderos.js
@@ -172,87 +172,57 @@ $ganaderos_list.on('click', 'tr td a.btn_delete_ganadero', function(e){
 
   });
 
-function populate_ganaderos(){
-  $.getJSON('http://localhost:3000/users/admin/list_ganaderos', function(data) {
-    ganaderos_array = data.ganaderos;
-    locations_array = data.locations;
-    console.log(locations_array);
-    var firstElement = ganaderos_array[0];
+  function populate_ganaderos(){
+    $.getJSON('http://localhost:3000/users/admin/list_ganaderos', function(data) {
+      ganaderos_array = data.ganaderos;
+      locations_array = data.locations;
+      console.log(locations_array);
+      var firstElement = ganaderos_array[0];
 
-    // contents of ganaderos list
-    var table_content = '';
+      // contents of ganaderos list
+      var table_content = '';
 
-    // for each item in JSON, add table row and cells
-    $.each(data.ganaderos, function(i){
-      table_content += '<tr>';
-      table_content += "<td><a class='list-group-item ";
-      // if initial list item, set to active
-      if(i==0) {
-        table_content +=  'active ';
-      }
-      table_content += "show_info_ganadero' href='#', data-id='"+this.person_id+"'>"+this.first_name+' '+this.last_name1+' '+this.last_name2+"</a></td>";
-      table_content += "<td><button class='btn_edit_ganadero btn btn-sm btn-success btn-block' type='button' data-id='"+this.person_id+"'>Editar</button></td>";
-      table_content += "<td><a class='btn_delete_ganadero btn btn-sm btn-success' data-toggle='tooltip' type='button' href='#' data-id='"+this.person_id+"'><i class='glyphicon glyphicon-trash'></i></a></td>";
-      table_content += '</tr>';
-    });  
-
-    // inject content string into html
-    $ganaderos_list.html(table_content);
-
-    //populate info panel with information regarding the first person on the list
-    $('#info_panel_heading').text(firstElement.first_name + " " + firstElement.last_name1 + " " + firstElement.last_name2);
-    if(firstElement.middle_initial == null) {
-      $('#ganadero_info_name').text(firstElement.first_name);
-    } else {
-      $('#ganadero_info_name').text(firstElement.first_name + " " + firstElement.middle_initial);
-    }
-    $('#ganadero_info_apellidos').text(firstElement.last_name1 + " " + firstElement.last_name2);
-    $('#ganadero_info_contact').text(firstElement.email + " " + firstElement.phone_number);
-
-    table_content = '';
-
-    $.each(locations_array, function(i){
-      if(firstElement.person_id == this.person_id){
-        console.log(firstElement.person_id);
-        console.log(this.person_id);
+      // for each item in JSON, add table row and cells
+      $.each(data.ganaderos, function(i){
         table_content += '<tr>';
-        table_content += "<td> ";
-        table_content += "" +this.location_name+"</td>";
+        table_content += "<td><a class='list-group-item ";
+        // if initial list item, set to active
+        if(i==0) {
+          table_content +=  'active ';
+        }
+        table_content += "show_info_ganadero' href='#', data-id='"+this.person_id+"'>"+this.first_name+' '+this.last_name1+' '+this.last_name2+"</a></td>";
+        table_content += "<td><button class='btn_edit_ganadero btn btn-sm btn-success btn-block' type='button' data-id='"+this.person_id+"'>Editar</button></td>";
+        table_content += "<td><a class='btn_delete_ganadero btn btn-sm btn-success' data-toggle='tooltip' type='button' href='#' data-id='"+this.person_id+"'><i class='glyphicon glyphicon-trash'></i></a></td>";
         table_content += '</tr>';
+      });  
+
+      // inject content string into html
+      $ganaderos_list.html(table_content);
+
+      //populate info panel with information regarding the first person on the list
+      $('#info_panel_heading').text(firstElement.first_name + " " + firstElement.last_name1 + " " + firstElement.last_name2);
+      if(firstElement.middle_initial == null) {
+        $('#ganadero_info_name').text(firstElement.first_name);
+      } else {
+        $('#ganadero_info_name').text(firstElement.first_name + " " + firstElement.middle_initial);
       }
-    });  
+      $('#ganadero_info_apellidos').text(firstElement.last_name1 + " " + firstElement.last_name2);
+      $('#ganadero_info_contact').text(firstElement.email + " " + firstElement.phone_number);
 
-    $('#ganadero_locations').html(table_content);
-   });
-};
+      table_content = '';
 
-function populate_info(){
-  $.ajax({
-    url: "http://localhost:3000/users/admin/ganadero_show",
-    method: "GET",
+      $.each(locations_array, function(i){
+        if(firstElement.person_id == this.person_id){
+          console.log(firstElement.person_id);
+          console.log(this.person_id);
+          table_content += '<tr>';
+          table_content += "<td> ";
+          table_content += "" +this.location_name+"</td>";
+          table_content += '</tr>';
+        }
+      });  
 
-    success: function( data ) {
-
-    },
-
-    // Code to run if the request fails; the raw request and
-    // status codes are passed to the function
-    error: function( xhr, status, errorThrown ) {
-      alert( "Sorry, there was a problem!" );
-      console.log( "Error: " + errorThrown );
-      console.log( "Status: " + status );
-      console.dir( xhr );
-    },
-
-    // Code to run regardless of success or failure
-    complete: function( xhr, status ) {
-      alert( "The request is complete!" );
-    }
-  });
-};
-
-function populate_locations(){
-  $.getJSON();
-};
-
+      $('#ganadero_locations').html(table_content);
+    });
+  };
 });
