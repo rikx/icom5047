151576eb5e519fcc7b61eb32202a25758e6bb23d
Rick Fuentes <ricardo.fuentes@upr.edu>
2015-04-24 02:44:46 -0400

    Testing out some things in edit cuestionario

--- Web/sea_server/public/javascripts/cuestionario.js
+++ Web/sea_server/public/javascripts/cuestionario.js
@@ -1,3 +1,50 @@
+jsPlumb.ready(function() {
+ 
+  jsPlumb.setContainer($('#container_jsplumb'));
+ 	
+ 	// variable to track # of states
+  var i = 0;
+ 
+  $('#container_jsplumb').dblclick(function(e) {
+    var newState = $('<div>').attr('id', 'state' + i).addClass('item');
+    
+    var title = $('<div>').addClass('title').text('State ' + i);
+    var connect = $('<div>').addClass('connect');
+    
+/*    newState.css({
+      'top': e.pageY,
+      'left': e.pageX
+    });*/
+    
+    newState.append(title);
+    newState.append(connect);
+    
+    $('#container_jsplumb').append(newState);
+ 
+    jsPlumb.makeTarget(newState, {
+      anchor: 'Continuous'
+    });
+    
+    jsPlumb.makeSource(connect, {
+      parent: newState,
+      anchor: 'Continuous'
+    });
+
+		jsPlumb.draggable(newState, {
+		  containment: 'parent'
+		});
+		 
+		newState.dblclick(function(e) {
+		  jsPlumb.detachAllConnections($(this));
+		  $(this).remove();
+		  e.stopPropagation();
+		});
+    
+    // increase id number to ensure uniqueness
+    i++;   
+  });  
+});
+
 $(document).ready(function(){
   /* Button: Return home */
 	$('#btn_home').on('click', function(){
