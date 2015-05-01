2a50a9e009aafeb0a51a3684d31cebee58491728
Rick Fuentes <ricardo.fuentes@upr.edu>
2015-05-01 02:52:23 -0400

    Further add/edit appointment enhancements to report page

--- Web/sea_server/public/javascripts/reporte.js
+++ Web/sea_server/public/javascripts/reporte.js
@@ -1,13 +1,8 @@
 $(document).ready(function(){
-	// answered questions list
-
-	var titleText = $('<input>').attr('type', 'text');
-	$answered_survey_panel = $('#answered_survey');
+  // Ramon code
+  var titleText = $('<input>').attr('type', 'text');
 	$('#btn_submit_notes').hide();
-	$('#btn_submit_appointment').hide();
-	$('#form_post_appointment').hide();
   $('#title_panel').hide();
-
 
   //double click title to display title edit form
   $('#title').dblclick(function(e) {
@@ -15,18 +10,16 @@ $(document).ready(function(){
     $('#title').hide();
   }); 
 
-
   // user_data is of the currently signed in user
   var user_data = JSON.parse($('#info_current_user').attr('data-user'));
   var report_data = JSON.parse($('#info_report').attr('data-report'));
-  var survey_data = JSON.parse($answered_survey_panel.attr('data-survey'));
+  var survey_data = JSON.parse($('#answered_survey').attr('data-survey'));
 
-  // checks if there is an appointment
-  var appointment_data = $('#citas').attr('data-appointment');
+  // stores appointment data locally if it exists
+  var appointment_data = $('#info_panel').attr('data-appointment');
   if(appointment_data != undefined) {
-    appointment_data = JSON.parse($('#citas').attr('data-appointment'));
+    appointment_data = JSON.parse($('#info_panel').attr('data-appointment'));
   }
-
 
   /* Button: Return home */
   $('#btn_home').on('click', function(){
@@ -38,7 +31,13 @@ $(document).ready(function(){
     window.location.href = '/users/reportes'; //TODO CHANGE manejar reportes so this works
   });
 
+  /* Close edit panel */
+  $('#btn_close_edit_panel').on('click', function(){
+    $('#edit_panel').hide();
+    $('#info_panel').show();
+  });
 
+  /* Edit note */
   $('#btn_notes').on('click', function(){
   	var currentText = $('#notas').text();
   	$('#btn_notes').hide();
@@ -46,6 +45,7 @@ $(document).ready(function(){
   	$('#notas').html("<textarea id='note_text' rows='10' cols='40'>" + currentText + "</textarea>");
   });
 
+  /* Submit edited note */
   $('#btn_submit_notes').on('click', function(){
   	$('#btn_notes').show();
   	$('#btn_submit_notes').hide();
@@ -77,46 +77,19 @@ $(document).ready(function(){
    });
   });
 
-  $('#btn_appointment').on('click', function(){
-    $('#no_appointment_text').hide();
-    $('#form_post_appointment').show();
-    $('#btn_appointment').hide();
-    $('#btn_submit_appointment').show();
-    
-    // if there is apointment data, display it in the edit fields
-    if(appointment_data != undefined){
-      // date formatting
-      var cita_date = appointment_data.date;
-      var cita_day = cita_date.slice(0,2);
-      var cita_month = cita_date.slice(3,5);
-      var cita_year = cita_date.slice(6,10)
-
-      cita_date = cita_year+"-"+cita_month+"-"+cita_day;
-      
-      // time formatting
-      var cita_time = appointment_data.time;
-      var cita_hour = cita_time.slice(0,2);
-      var cita_minutes = cita_time.slice(3,5)
-      var cita_meridian = cita_time.slice(6,8);
-      // if time is pm, convert to military time
-      if(cita_meridian == 'PM'){
-        cita_hour = parseInt(cita_hour) + 12;
-      } 
-      cita_time = cita_hour+':'+cita_minutes;
-
-      // set edit forms with existing values
-      $('#cita_date').val(cita_date);
-      $('#cita_time').val(cita_time);
-      $('#cita_purpose').val(appointment_data.purpose);
-    }
+  /* Add appointment */
+  $('#btn_add_appointment').on('click', function(){
+    $('#info_panel').hide();
+    $('#edit_panel').show();
   });
 
+  /* Submit new appointment */
   $('#btn_submit_appointment').on('click', function(){
     var $the_form = $('#form_post_appointment');
     var form_data = $the_form.serializeArray();
     var new_appointment = ConverToJSON(form_data);
 
-    // ajax call to update location
+    // ajax call to post new appointment
     $.ajax({
       url: "http://localhost:3000/users/appointment/" + report_data.report_id + "/" + user_data.user_id,
       method: "POST",
@@ -126,29 +99,23 @@ $(document).ready(function(){
 
       success: function(data) {
         alert("Se ha agregado una nueva cita.");
-        var this_appointment = data.appointment;
+        appointment_data = data.appointment;
+        appointment_data.maker = user_data.username;
 
         var citas_content = '';
 
-        citas_content += "<p><strong>Creador de cita: </strong><span id='appointment_maker'>"+user_data.username+"</span></p>";
-        citas_content += "<p><strong>Fecha: </strong><span id='appointment_date'>"+this_appointment.date+"</span></p>";
-        citas_content += "<p><strong>Hora: </strong><span id='appointment_time'>"+this_appointment.time+"</span></p>";
-        citas_content += "<p><strong> Propósito: </strong><span id='appointment_purpose'>"+this_appointment.purpose+"</span></p>";
-
-        // hide post forms
-        $('#btn_submit_appointment').hide();
-        $('#form_post_appointment').hide();
-        $('#btn_appointment').show();
+        citas_content += "<p><strong>Creador de cita: </strong><span id='appointment_maker'>"+appointment_data.maker+"</span></p>";
+        citas_content += "<p><strong>Fecha: </strong><span id='appointment_date'>"+appointment_data.date+"</span></p>";
+        citas_content += "<p><strong>Hora: </strong><span id='appointment_time'>"+appointment_data.time+"</span></p>";
+        citas_content += "<p><strong> Propósito: </strong><span id='appointment_purpose'>"+appointment_data.purpose+"</span></p>";
+        citas_content += "<a id='btn_edit_appointment' class='btn btn-sm btn-success' data-toggle='tooltip' type='button' href='#' data-id='"+appointment_data.appointment_id+"'><i class='glyphicon glyphicon-edit'></i></a>";
 
         // inject html string
-        $('#citas').html(citas_content);
+        $('#info_panel').html(citas_content);
 
-        // for appointment update
-/*        // set cita information in panel
-        $('#appointment_date').val(this_appointment.date);
-        $('#appointment_time').val(this_appointment.time);
-        $('#appointment_purpose').val(this_appointment.purpose);
-        $('#appointment_maker').val(user_data.username);*/
+        // hide post form; display info panel
+        $('#edit_panel').hide();
+        $('#info_panel').show();
       },
       error: function( xhr, status, errorThrown ) {
         alert( "Sorry, there was a problem!" );
@@ -157,6 +124,39 @@ $(document).ready(function(){
         console.dir( xhr );
       }
     });
+  });
+
+  /* Edit appointment */
+  $('#info_panel').on('click', 'a#btn_edit_appointment', function(e){
+    // prevents link from firing
+    e.preventDefault();
+
+    $('#info_panel').hide();
+    $('#edit_panel').show();
+
+    // date formatting
+    var cita_date = appointment_data.date;
+    var cita_day = cita_date.slice(0,2);
+    var cita_month = cita_date.slice(3,5);
+    var cita_year = cita_date.slice(6,10)
+
+    cita_date = cita_year+"-"+cita_month+"-"+cita_day;
+    
+    // time formatting
+    var cita_time = appointment_data.time;
+    var cita_hour = cita_time.slice(0,2);
+    var cita_minutes = cita_time.slice(3,5)
+    var cita_meridian = cita_time.slice(6,8);
+    // if time is pm, convert to military time
+    if(cita_meridian == 'PM'){
+      cita_hour = parseInt(cita_hour) + 12;
+    } 
+    cita_time = cita_hour+':'+cita_minutes;
+
+    // set edit forms with existing values
+    $('#cita_date').val(cita_date);
+    $('#cita_time').val(cita_time);
+    $('#cita_purpose').val(appointment_data.purpose);
   });
 
   $('#btn_submit_title').on('click', function(){
