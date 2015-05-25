3d5d5bcc04a2f55d79bef95c70bce49239328b21
nreyesciena <nelson.reyes@upr.edu>
2015-05-25 03:09:09 -0400

    GCM Client updated

--- Android/SEA/gcm-client/src/main/java/com/rener/gcm_client/MainActivity.java
+++ Android/SEA/gcm-client/src/main/java/com/rener/gcm_client/MainActivity.java
@@ -16,10 +16,6 @@ import java.io.IOException;
 public class MainActivity extends Activity implements View.OnClickListener {
 
 	Button btnRegId;
-	EditText etRegId;
-	GoogleCloudMessaging gcm;
-	String regid;
-	String PROJECT_NUMBER = "AIzaSyDAUpf_GpRVw4DQBnz7pr_aGzDEFoWJ3_8";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,35 +23,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		setContentView(R.layout.activity_main);
 
 		btnRegId = (Button) findViewById(R.id.btnGetRegId);
-		etRegId = (EditText) findViewById(R.id.etRegId);
 
 		btnRegId.setOnClickListener(this);
 	}
 	public void getRegId(){
-		new AsyncTask<Void, Void, String>() {
-			@Override
-			protected String doInBackground(Void... params) {
-				String msg = "";
-				try {
-					if (gcm == null) {
-						gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
-					}
-					regid = gcm.register(PROJECT_NUMBER);
-					msg = "Device registered, registration ID=" + regid;
-					Log.i("GCM", msg);
-
-				} catch (IOException ex) {
-					msg = "Error :" + ex.getMessage();
-
-				}
-				return msg;
-			}
-
-			@Override
-			protected void onPostExecute(String msg) {
-				etRegId.setText(msg + "\n");
-			}
-		}.execute(null, null, null);
+		new GcmRegistrationAsyncTask(this).execute();
 	}
 	@Override
 	public void onClick(View v) {
